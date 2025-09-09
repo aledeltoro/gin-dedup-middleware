4705b170986b7924d73e4dc0b99966d3891efaa3
Alejandro Encarnación <aencarnacion@deuna.com>
2025-09-09 11:59:57 -0400

    update signature of AddSet method to take multiple members for a single set key

--- storage/cache.go
+++ storage/cache.go
@@ -6,6 +6,6 @@ import (
 )
 
 type CacheStorage interface {
-	AddSet(ctx context.Context, key string, member string, expire time.Duration) error
+	AddSet(ctx context.Context, key string, expire time.Duration, members ...string) error
 	IsSetMember(ctx context.Context, key, member string) (bool, error)
 }
